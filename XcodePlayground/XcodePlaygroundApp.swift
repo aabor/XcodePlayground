db5829ac2471922299d8dc6a3e4d3ee531d39a2f
Alexander Borochkin <aabor.nn.com@gmail.com>
2025-10-08 09:47:38 -0700

    Initial Commit

---
+++ XcodePlayground/XcodePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodePlaygroundApp.swift
+//  XcodePlayground
+//
+//  Created by Alexander Borochkin on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
